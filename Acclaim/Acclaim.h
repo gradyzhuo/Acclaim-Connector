a49a1e18ee7e8cf7072cc6f7d407d9cd7eae9e0d
Grady Zhuo <gradyzhuo@gmail.com>
2015-08-12 12:36:22 +0800

    Initial Commit

---
+++ Acclaim/Acclaim.h
@@ -0,0 +1,19 @@
+//
+//  Acclaim.h
+//  Acclaim
+//
+//  Created by Grady Zhuo on 8/12/15.
+//  Copyright (c) 2015 Grady Zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Acclaim.
+FOUNDATION_EXPORT double AcclaimVersionNumber;
+
+//! Project version string for Acclaim.
+FOUNDATION_EXPORT const unsigned char AcclaimVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Acclaim/PublicHeader.h>
+
+
