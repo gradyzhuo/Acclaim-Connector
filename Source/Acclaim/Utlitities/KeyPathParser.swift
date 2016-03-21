1b8ee36ef7deac6da46b1957ff7ca014e808a0c0
Grady Zhuo <gradyzhuo@gmail.com>
2016-03-21 14:28:30 +0800

    [KeyPath] create struct KeyPath to handle keyPath.

--- Source/Acclaim/Utlitities/KeyPathParser.swift
+++ Source/Acclaim/Utlitities/KeyPathParser.swift
@@ -8,7 +8,51 @@
 
 import Foundation
 
+public struct KeyPath {
+    public typealias ExpectedType = String
     
+    public internal(set) var path: String
+    public internal(set) var separater:String
+    
+    public init(path: String, separater:String = "."){
+        self.path = path
+        self.separater = separater
+    }
+    
+}
+
+extension KeyPath : Hashable {
+    public var hashValue: Int{
+        return self.path.hash
+    }
+}
+
+public func ==(lhs: KeyPath, rhs: KeyPath)->Bool{
+    return lhs.path == rhs.path
+}
+
+extension KeyPath : StringLiteralConvertible {
+    
+    public typealias StringLiteralType = String
+    public typealias ExtendedGraphemeClusterLiteralType = String
+    public typealias UnicodeScalarLiteralType = String
+    
+    public init(stringLiteral value: StringLiteralType) {
+        self = KeyPath(path: value, separater: ".")
+        
+    }
+    
+    /// Create an instance initialized to `value`.
+    public init(extendedGraphemeClusterLiteral value: ExtendedGraphemeClusterLiteralType) {
+        self.init(stringLiteral: value)
+    }
+    
+    /// Create an instance initialized to `value`.
+    public init(unicodeScalarLiteral value: UnicodeScalarLiteralType) {
+        self.init(stringLiteral: value)
+    }
+    
+}
 
 
 public protocol KeyPathParser{
